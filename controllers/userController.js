d701dd5313c0be340005a1f4f7bde64da2ade4ed
lakshyavijh <lakshya.vijh99@gmail.com>
2020-02-06 00:23:55 +0530

    updating post in db

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,7 +15,7 @@ exports.mustBeLoggedIn = function(req, res, next) {
 exports.login = function(req, res) {
   let user = new User(req.body)
   user.login().then(function(result) {
-    req.session.user = {avatar: user.avatar, username: user.data.username,_id : user.data._id}
+    req.session.user = {avatar: user.avatar, username: user.data.username, _id: user.data._id}
     req.session.save(function() {
       res.redirect('/')
     })
@@ -36,7 +36,7 @@ exports.logout = function(req, res) {
 exports.register = function(req, res) {
   let user = new User(req.body)
   user.register().then(() => {
-    req.session.user = {username: user.data.username, avatar: user.avatar, _id : user.data._id}
+    req.session.user = {username: user.data.username, avatar: user.avatar, _id: user.data._id}
     req.session.save(function() {
       res.redirect('/')
     })
@@ -54,32 +54,29 @@ exports.home = function(req, res) {
   if (req.session.user) {
     res.render('home-dashboard')
   } else {
-    res.render('home-guest', {errors: req.flash('errors'), regErrors: req.flash('regErrors')})
+    res.render('home-guest', {regErrors: req.flash('regErrors')})
   }
 }
 
-exports.ifUserExists = function(req,res,next) {
-  User.findByUsername(req.params.username).then(function(userDocument){
+exports.ifUserExists = function(req, res, next) {
+  User.findByUsername(req.params.username).then(function(userDocument) {
     req.profileUser = userDocument
     next()
   }).catch(function() {
-    res.render('404')
+    res.render("404")
   })
-
 }
 
-exports.profilePostsScreen = function(req,res) {
-  // ask our post model by certain author id
+exports.profilePostsScreen = function(req, res) {
+  // ask our post model for posts by a certain author id
   Post.findByAuthorId(req.profileUser._id).then(function(posts) {
     res.render('profile', {
-      posts : posts,
-      profileUsername : req.profileUser.username,
-      profileAvatar : req.profileUser.avatar
+      posts: posts,
+      profileUsername: req.profileUser.username,
+      profileAvatar: req.profileUser.avatar
     })
-  }).catch(function(){
-    res.render('404')
+  }).catch(function() {
+    res.render("404")
   })
-
-  
 
 }
--- models/Post.js
+++ models/Post.js
@@ -2,10 +2,11 @@ const postsCollection = require('../db').db().collection("posts")
 const ObjectID = require('mongodb').ObjectID
 const User = require('./User')
 
-let Post = function(data, userid) {
+let Post = function(data, userid, requestedPostId) {
   this.data = data
   this.errors = []
   this.userid = userid
+  this.requestedPostId = requestedPostId
 }
 
 Post.prototype.cleanUp = function() {
@@ -44,7 +45,37 @@ Post.prototype.create = function() {
   })
 }
 
-Post.reusablePostQuery = function(uniqueOperations,visitorId) {
+Post.prototype.update = function() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let post = await Post.findSingleById(this.requestedPostId, this.userid)
+      if (post.isVisitorOwner) {
+        // actually update the db
+        let status = await this.actuallyUpdate()
+        resolve(status)
+      } else {
+        reject()
+      }
+    } catch {
+      reject()
+    }
+  })
+}
+
+Post.prototype.actuallyUpdate = function() {
+  return new Promise(async (resolve, reject) => {
+    this.cleanUp()
+    this.validate()
+    if (!this.errors.length) {
+      await postsCollection.findOneAndUpdate({_id: new ObjectID(this.requestedPostId)}, {$set: {title: this.data.title, body: this.data.body}})
+      resolve("success")
+    } else {
+      resolve("failure")
+    }
+  })
+}
+
+Post.reusablePostQuery = function(uniqueOperations, visitorId) {
   return new Promise(async function(resolve, reject) {
     let aggOperations = uniqueOperations.concat([
       {$lookup: {from: "users", localField: "author", foreignField: "_id", as: "authorDocument"}},
@@ -56,12 +87,13 @@ Post.reusablePostQuery = function(uniqueOperations,visitorId) {
         author: {$arrayElemAt: ["$authorDocument", 0]}
       }}
     ])
-    
+
     let posts = await postsCollection.aggregate(aggOperations).toArray()
 
     // clean up author property in each post object
     posts = posts.map(function(post) {
       post.isVisitorOwner = post.authorId.equals(visitorId)
+
       post.author = {
         username: post.author.username,
         avatar: new User(post.author, true).avatar
@@ -69,17 +101,19 @@ Post.reusablePostQuery = function(uniqueOperations,visitorId) {
 
       return post
     })
+
     resolve(posts)
   })
 }
 
-Post.findSingleById = function(id,visitorId) {
+Post.findSingleById = function(id, visitorId) {
   return new Promise(async function(resolve, reject) {
     if (typeof(id) != "string" || !ObjectID.isValid(id)) {
       reject()
       return
     }
-    let posts =await Post.reusablePostQuery([
+    
+    let posts = await Post.reusablePostQuery([
       {$match: {_id: new ObjectID(id)}}
     ], visitorId)
 
@@ -95,7 +129,8 @@ Post.findSingleById = function(id,visitorId) {
 Post.findByAuthorId = function(authorId) {
   return Post.reusablePostQuery([
     {$match: {author: authorId}},
-    {$sort: {createdDate : -1}}
+    {$sort: {createdDate: -1}}
   ])
 }
+
 module.exports = Post
